394300573d2da949002c4266e889cacc9ad7fcea
Juri Tomak <juri.tomak@gselectronic.com>
2023-07-07 20:14:15 +0200

    use setup.cfg instead of setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,2 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="rast_common_python",
-    version="1.0.0",
-    author="Juri Tomak",
-    author_email="jtomak@uni-muenster.de",
-    license='MIT',
-    description="Python classes and functions used in multiple components of our RAST implementation.",
-    url="https://github.com/your-username/your-package",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-    ],
-)
+import setuptools
+setuptools.setup()
--- src/rast_common/__init__.py
+++ src/rast_common/__init__.py
@@ -1,2 +1,3 @@
 from StringUtils import *
 from SwitchAggFlowStats import *
+from FileUtils import *
