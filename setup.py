273a1403816cd486ee4a506918d7a802c339084b
Juri Tomak <juri.tomak@gselectronic.com>
2023-07-07 19:30:38 +0200

    adopted src-layout

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="RAST-Common-Python",
+    name="rast_common_python",
     version="1.0.0",
     author="Juri Tomak",
     author_email="jtomak@uni-muenster.de",
+    license='MIT',
     description="Python classes and functions used in multiple components of our RAST implementation.",
     url="https://github.com/your-username/your-package",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
