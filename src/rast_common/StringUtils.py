62c89c0803c1a85233fd83ba22275bffff1f432f
Juri Tomak <juri.tomak@gselectronic.com>
2023-07-07 19:39:07 +0200

    added remaining string utility functions from Common.py

--- src/rast_common/StringUtils.py
+++ src/rast_common/StringUtils.py
@@ -1,5 +1,35 @@
-import re
+import argparse
+import os
+from datetime import datetime
+from re import search
 
 
 def get_date_from_string(line):
-    return re.search(r"\d*-\d*-\d*", line).group().strip()
+    return search(r"\d*-\d*-\d*", line).group().strip()
+
+
+def contains_timestamp_with_ms(line: str):
+    return search(r"(?<=\])\s*\d*-\d*-\d*\s\d*:\d*:\d*\.\d*", line) is not None
+
+
+def get_timestamp_from_string(line: str):
+    return search(r"(?<=\])\s*\d*-\d*-\d*\s\d*:\d*:\d*\.?\d*", line).group().strip()
+
+
+def get_timestamp_from_line(line: str) -> datetime:
+    if contains_timestamp_with_ms(line):
+        format_string = '%Y-%m-%d %H:%M:%S.%f'
+    else:
+        format_string = '%Y-%m-%d %H:%M:%S'
+
+    return datetime.strptime(
+        get_timestamp_from_string(line),
+        format_string
+    )
+
+
+def dir_path(path):
+    if os.path.isdir(path):
+        return path
+    else:
+        raise argparse.ArgumentTypeError(f"readable_dir:{path} is not a valid path")
